847ae8a9682576340fe49ee49eead82542fbc385
ThePython10110 <87204246+ThePython10110@users.noreply.github.com>
2022-11-24 12:07:56 -0800

    Improved batch convert script further, added comments, added MSCX and MXL formats.\nUnfortunately, I had to do a full batch convert because I interrupted one and didn't want to break anything.

--- batch_convert.py
+++ batch_convert.py
@@ -1,53 +1,36 @@
 import os, json, subprocess
 
-filetypes = ["MP3", "MIDI", "PDF"]
+filetypes = ["MP3", "MIDI", "PDF", "MSCX", "MXL"]                   #https://musescore.org/en/handbook/3/command-line-options#Batch_conversion_job_JSON_format for a list of file types
 
 json_output = []
 
-with open("last_modified.json", "r") as json_file:
-    old_changes = json.load(json_file)
-
-new_changes = old_changes
-
-for root, dirs, files in os.walk("MuseScore"):
-    for name in files: 
-        if name.endswith(".mscz") and "Private" not in root: #I'm not making EVERYTHING public... :D
-            no_extension = name[:-5]
-            path = os.path.join(root, name)
-            last_modified = os.path.getmtime(path)
-            try:
-                if last_modified != old_changes[no_extension]:
-                    new_changes[no_extension] = last_modified
-                    filenames = []
-                    for filetype in filetypes:
-                        new_dir = root.replace("MuseScore", filetype, 1)
-                        if not os.path.exists(new_dir):
-                            os.makedirs(new_dir)
-                        new_filename = os.path.join(new_dir, no_extension + "." + filetype.lower())
-                        filenames.append(new_filename)
-                    json_output.append({
-                        "in": path,
-                        "out": filenames
-                    })
-            except KeyError:
-                new_changes[no_extension] = last_modified
-                filenames = []
-                for filetype in filetypes:
-                    new_dir = root.replace("MuseScore", filetype, 1)
-                    if not os.path.exists(new_dir):
-                        os.makedirs(new_dir)
-                    new_filename = os.path.join(new_dir, no_extension + "." + filetype.lower())
-                    filenames.append(new_filename)
-                json_output.append({
+for root, dirs, files in os.walk("MuseScore"):                      #For every item in the MuseScore folder:
+    for name in files: #For every file:
+        if name.endswith(".mscz") and "Private" not in root:        #If it's a .mscz file and not in any folder called "Private" (I'm not making EVERYTHING public... :D)
+            no_extension = name[:-5]                                #Get the filename without an extension (since I know the extension is ".mscz", I can just grab everything but the last 5 character)
+            path = os.path.join(root, name)                         #The path to the file (MuseScore\HFF\Tomorrow.mscz for example)
+            filenames = []
+            score_modified = os.path.getmtime(path)                 #Get the time the score was modified
+            for filetype in filetypes:                              #For every filetype:
+                new_dir = root.replace("MuseScore", filetype, 1)    #Replace the first instance of "MuseScore" in the path with the filetype (MuseScore\HFF would become PDF\HFF, for example)
+                if not os.path.exists(new_dir):                     #If the folder doesn't exist:
+                    os.makedirs(new_dir)                            #Create it.
+                new_filename = os.path.join(new_dir, no_extension + "." + filetype.lower()) #Generate the new file path (such as PDF\HFF\Tomorrow.pdf)
+                file_modified = 0                                   #Setting this to zero, so if the file doesn't exist, it will assume it was modified at the epoch date
+                try:
+                    file_modified = os.path.getmtime(new_filename)  #Get the modify date of the converted file
+                except FileNotFoundError:                           #The file may not exist yet
+                    pass
+                if score_modified > file_modified:                  #If the score has been modified since the last conversion:
+                    filenames.append(new_filename)                  #Add it to the list of files to convert
+            if filenames:                                           #If there is anything to convert:
+                json_output.append({                                #Add it to the JSON output
                     "in": path,
                     "out": filenames
                 })
 
 
-with open("convert_job.json", "w") as json_file:
+with open("convert_job.json", "w") as json_file:                    #Write to the JSON file
     json.dump(json_output, json_file, indent=2)
 
-subprocess.call(r'"C:\Program Files\MuseScore 3\bin\MuseScore3.exe" -j convert_job.json')
-
-with open("last_modified.json", "w+") as json_file:
-    json.dump(new_changes, json_file, indent=2)
+subprocess.call(r'"C:\Program Files\MuseScore 3\bin\MuseScore3.exe" -j convert_job.json') #Convert!
